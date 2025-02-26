1106f6e0ca27b11bce15e65856e1d0705bfaab15
Miroslav-Ivanov <43472264+Miroslav-Ivanov@users.noreply.github.com>
2025-02-26 21:48:50 +0000

    Update gendata.py
    Int division operator used ( "//")

--- scripts/gendata.py
+++ scripts/gendata.py
@@ -31,11 +31,9 @@ with open(selfdata_c_path, "w") as output:
                  "__attribute__ ((aligned (4))) = {")
     crcs = []
     crc = 0
-    type(bootloader_size)
-    for int(row) in range(bootloader_size / 16):
-        type(row)
+    for row in range(bootloader_size // 16):
         # Save the crc every 1k.
-        if row % (1024 / 16) == 0 and row > 0:
+        if row % (1024 // 16) == 0 and row > 0:
             crcs.append(crc)
             crc = 0
         start_index = row * 16
